8f8a7c68d48045a10c860ff4be0f5364cabc0259
Radu-A <elcastravete@gmail.com>
2023-07-24 17:11:50 +0200

    changed/api_key

--- client/src/components/Header/Temperature/Temperature.jsx
+++ client/src/components/Header/Temperature/Temperature.jsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from "react";
 
 const Temperature = () => {
-
   const [weather, setWeather] = useState({});
 
-  useEffect(()=>{
+  useEffect(() => {
     if (navigator.geolocation) {
-
       navigator.geolocation.getCurrentPosition(async (pos) => {
-  
         let { latitude: lat, longitude: lon } = pos.coords;
-        
+
         try {
-          let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=d96ac8728492b4bae1caf1e47f7501d8`);
+          let res = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=7dbe1f4a3cf59cbe6938cf92bc5d1778`
+          );
           const data = await res.json();
           setWeather(data);
           console.log(data);
@@ -21,7 +20,7 @@ const Temperature = () => {
         }
       });
     }
-  }, [])
+  }, []);
 
   return (
     <>
