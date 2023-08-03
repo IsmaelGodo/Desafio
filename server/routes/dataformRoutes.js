6949050876666a8d12607b2b0a5f5684379b2f0b
Radu-A <elcastravete@gmail.com>
2023-08-04 00:40:52 +0200

    fixed/routes

--- server/routes/dataformRoutes.js
+++ server/routes/dataformRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const dataformController = require("../controllers/dataformController");
-const authMiddlewares = require('../middlewares/authMiddlewares')
+const authMiddlewares = require("../middlewares/authMiddlewares");
 const dataformRouter = express.Router();
 
 dataformRouter.get("/", dataformController.getDataform);
-dataformRouter.post("/", authMiddlewares.checkToken, dataformController.postDataform);
-dataformRouter.put("/", authMiddlewares.checkToken, dataformController.putDataform);
+dataformRouter.post("/", dataformController.postDataform);
+dataformRouter.put("/", dataformController.putDataform);
 
 module.exports = dataformRouter;
